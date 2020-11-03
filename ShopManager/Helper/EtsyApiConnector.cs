0444ddb17efcd0afdfb8f9942f7d5fd536a3a88e
GandrLu <acidyumyum@web.de>
2020-11-03 13:45:07 +0100

    Add OAuth etsy authentification.

---
+++ ShopManager/Helper/EtsyApiConnector.cs
@@ -0,0 +1,30 @@
+﻿using OAuth;
+using ShopManager.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManager.Helper
+{
+    class EtsyApiConnector
+    {
+        private const string CONSUMER_KEY = "fill in key";
+        private const string CONSUMER_SECRET = "fill in secret";
+
+        public EtsyApiConnector()
+        {
+            var oAuth = new OAuth.Manager();
+            string requestUrl = "https://openapi.etsy.com/v2/oauth/request_token?scope=email_r";
+            oAuth["consumer_key"] = CONSUMER_KEY;
+            oAuth["consumer_secret"] = CONSUMER_SECRET;
+            OAuthResponse tokenResponse = oAuth.AcquireRequestToken(requestUrl, "GET");
+            string unescapedAuthUrl = Uri.UnescapeDataString(tokenResponse.AllText);
+            unescapedAuthUrl = unescapedAuthUrl.Remove(0, "login_url=".Length);
+            System.Diagnostics.Process.Start(unescapedAuthUrl);
+        }
+    }
+}
